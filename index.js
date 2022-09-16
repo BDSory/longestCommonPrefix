d26d13d553f07d2b240ea5738fa814f315a6b534
Bryan S <superbee45@gmail.com>
2022-09-16 12:10:48 -0500

    zippy

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-/* Plain language: I need to compare the first letter of each string in the array and if they're all equal, push that into a new array. Then do it again on the second letter of each word, and if they're also all equal, push that onto the array. We should return the array when the comparison of string indices is not equal. 
-*/
+
 var longestCommonPrefix = function(strs) {
-  let finalArray = [];
-  let holdingTank = [];
-  let counter = 0;
-  for(let str of strs) {
-    
-    holdingTank.push(str[counter])
-    
+  
+  let r = '';
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) return r;
+    }
+    r += strs[0][i];
   }
-  const allEqual = arr => arr.every(val => val === arr[0]);
-  if(allEqual(holdingTank)) {
-    finalArray.push(holdingTank[0])
-  }
-  return finalArray;
-}
+  return r;
+
+};
