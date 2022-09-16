34ac23238657c3ab762bc923c625524ea19f5730
Bryan S <superbee45@gmail.com>
2022-09-16 09:15:16 -0500

    getting 1st element to array of commmon prefixes; fix counter

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 /* Plain language: I need to compare the first letter of each string in the array and if they're all equal, push that into a new array. Then do it again on the second letter of each word, and if they're also all equal, push that onto the array. We should return the array when the comparison of string indices is not equal. 
-1. loop over each str in strs (words)
-2. 
 */
 var longestCommonPrefix = function(strs) {
-  
+  let finalArray = [];
   let holdingTank = [];
   let counter = 0;
   for(let str of strs) {
@@ -12,5 +10,8 @@ var longestCommonPrefix = function(strs) {
     
   }
   const allEqual = arr => arr.every(val => val === arr[0]);
-  return allEqual(holdingTank);
+  if(allEqual(holdingTank)) {
+    finalArray.push(holdingTank[0])
+  }
+  return finalArray;
 }
